1ebba7133c19d7008266906232f9ea6f6ead2d53
Creeper Hunter <wijnbergduncan@gmail.com>
2016-04-27 14:05:35 -1000

    first commit
    publish for group

---
+++ app.js
@@ -0,0 +1,23 @@
+var Uscore = 0;
+
+var highscore = 15;
+
+var press = function() {
+	console.log('test');
+	Uscore++;
+	document.getElementById('score').innerHTML = "Cookies: " + Uscore;
+	check();
+
+};
+
+
+var check = function() {
+	if (Uscore > highscore) {
+		highscore = Uscore;
+		document.getElementById('highhhhhscore').innerHTML = "Highscore: " + highscore;
+	};
+}
+
+
+alert('Welcome To Bok Choi\'s Cookie Clicker. DO WUT U NEED TO DO OR THE ASIAN INVASION WILL FIND YOU!');
+
